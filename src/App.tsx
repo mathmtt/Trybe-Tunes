e4531dc5d669597150a38d3a695d2ae3a8e6311e
Matheus Motta <matheusmotta136@gmail.com>
2023-06-15 17:34:15 -0300

    Utilizando a função createUser para salvar as informações do usuário e redirecionar para a página de busca (requisito 01)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={ <Login /> } />
-      <Route path="/search" element={ <Search /> } />
-      <Route path="/album" element={ <Album /> } />
-      <Route path="*" element={ <NotFount /> } />
+      <Route path="/search" element={ <h1>Search</h1> } />
+      <Route path="/album" element={ <h1>Album</h1> } />
+      <Route path="*" element={ <h1>NotFound</h1> } />
     </Routes>
   );
 }
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,3 +1,65 @@
-import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
-function Login
+const init = {
+  name: '',
+};
+
+type Nome = {
+  name: string;
+};
+
+function Login() {
+  const [loading, setLoading] = useState(false);
+  const [disabledButton, setDisabledButton] = useState(true);
+  const traveling = useNavigate();
+  const [inputs, setInputs] = useState<Nome>(init);
+
+  const handleChange = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+    await createUser(inputs);
+    setLoading(false);
+    traveling('/search');
+  };
+
+  const handleClick = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputs({
+      ...inputs,
+      [event?.target.id]: event?.target.value,
+    });
+    if (inputs.name.length >= 2) {
+      setDisabledButton(false);
+    }
+  };
+
+  if (loading) {
+    return <Loading />;
+  }
+  return (
+    <div>
+      <form onSubmit={ handleChange }>
+        <div>
+          <label htmlFor="name">Insira seu nome:</label>
+          <input
+            type="text"
+            id="name"
+            data-testid="login-name-input"
+            value={ inputs.name }
+            onChange={ handleClick }
+          />
+        </div>
+        <button
+          data-testid="login-submit-button"
+          disabled={ disabledButton }
+        >
+          Entrar
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
